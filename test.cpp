2ebc4a550cff3c63c53fbb0e83eaf04ed9ad1f92
harshit-jain31 <169134156+harshit-jain31@users.noreply.github.com>
2024-06-13 13:31:41 +0530

    Create test.cpp

---
+++ test.cpp
@@ -0,0 +1,27 @@
+// Calculator.h
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+class Calculator {
+public:
+    int add(int a, int b);
+    int subtract(int a, int b);
+    int multiply(int a, int b);
+    double divide(int a, int b);
+};
+
+#endif // CALCULATOR_H
+
+// Calculator.h
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+class Calculator {
+public:
+    int add(int a, int b);
+    int subtract(int a, int b);
+    int multiply(int a, int b);
+    double divide(int a, int b);
+};
+
+#endif // CALCULATOR_H
