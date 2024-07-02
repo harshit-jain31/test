18f587a8512180edc67d40398e429d99c4b80744
harshit-jain31 <169134156+harshit-jain31@users.noreply.github.com>
2024-07-02 10:18:21 +0530

    Create getNextBill.js

---
+++ getNextBill.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const { validationResult } = require('express-validator');
+const { Utils } = require('dxl-dds-common');
+
+const {
+    handleValidationError,
+    httpMetricsOptions,
+    errorCounterMetrics,
+    handleApiError,
+    dbRequestDurationMetrics,
+    buildErrorObject
+} = require('../../utils/common');
+
+const CONSTANTS = require('../../config/constant');
+const handlers = require('../../handlers/v1/query-handlers');
+
+const getNextBill = (req, res, next) => {
+    res.locals.SORCOLLECTION = CONSTANTS.UTILITY_HISTORY_COLLECTION_NAME;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return handleValidationError(req, res, errors);
+    }
+    handlers.getNextBill(res, callbackHandler(req, res, next));
+};
+
+const callbackHandler = (req, res, next) => {
+    const callback = (err, result) => {
+        if (result) {
+            dbRequestDurationMetrics(
+                req,
+                res,
+                CONSTANTS.API_URI.v1.GET_NEXTBILL
+            );
+            res.locals.responseBody = result;
+            httpMetricsOptions(req, res, CONSTANTS.API_URI.v1.GET_NEXTBILL);
+            next();
+        } else {
+            res.locals.MongoDBTimeTaken = Utils.getEpochTime(
+                res.locals.startDBTime
+            );
+            let errorObj = buildErrorObject(err);
+            errorCounterMetrics(req, CONSTANTS.API_URI.v1.GET_NEXTBILL);
+            handleApiError(req, res, errorObj);
+        }
+    };
+    return callback;
+};
+
+module.exports = getNextBill;
