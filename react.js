78fc129c992c69600329ef4bc2503994083f9422
harshit-jain31 <169134156+harshit-jain31@users.noreply.github.com>
2024-07-11 15:45:22 +0530

    Update react.js

--- react.js
+++ react.js
@@ -1,42 +1,210 @@
 import { Link } from "react-router-dom";
+import MobilePhone from '../images/Mobile.jpg';
+import Laptop from '../images/Laptop.jpg';
+import Headset from '../images/Headphones.jpg';
+import Desktop from '../images/DesktopNew.jpg';
+import { useEffect, useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
+import { useNavigate } from "react-router-dom";
+function ElectronicSubCategory({setSearchedData}) {
+    const [ackData,setAckData] = useState();
+    const [catName,setCatName] = useState('');
+    const [finalData,setFinalData] = useState([]);
+    const [loader,setLoader] = useState(false);
+    const navigate = useNavigate();
+    var categoryItem = '';
 
-function Menu() {
+    const ackDataPrepare = async()=>{
+      setLoader(true);
+        console.log("catName",catName)
+        const uniqueId = uuidv4();
+        console.log(categoryItem,uniqueId)
+        let tempJosn ={
+            "context":
+            {
+              "domain":"ONDC:RET10",
+                "action":"search",
+                "country":"IND",
+                "city":"std:0172",
+                "core_version":"1.2.0",
+                "bap_id":"api.aabglaunchpadindiademo.com",
+                "bap_uri":"https://api.aabglaunchpadindiademo.com/bapl",
+                "transaction_id":uniqueId,
+                "message_id":"1655281254860",
+                "timestamp":"2023-02-19T15:28:00.000Z",
+                "ttl":"PT30S"
+            },
+            "message":
+            {
+              "intent":
+              {
+                "category":
+                {
+                  "id":categoryItem
+                },
+                "fulfillment":
+                {
+                  "type":"Delivery"
+                },
+                "payment":
+                {
+                  "@ondc/org/buyer_app_finder_fee_type":"percent",
+                  "@ondc/org/buyer_app_finder_fee_amount":"3"
+                },
+                "tags":
+                [
+                  {
+                    "code":"bap_terms",
+                    "list":
+                    [
+                      {
+                        "code":"static_terms",
+                        "value":""
+                      },
+                      {
+                        "code":"static_terms_new",
+                        "value":"https://github.com/ONDC-Official/NP-Static-Terms/buyerNP_BNP/1.0/tc.pdf"
+                      },
+                      {
+                        "code":"effective_date",
+                        "value":"2023-10-01T00:00:00.000Z"
+                      }
+                    ]
+                  }
+                ]
+              }
+            }
+          }
+          console.log(tempJosn)
+          const response = await fetch("https://api.aabglaunchpadindiademo.com/bapl/search", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(tempJosn)
+            })
+            const result = await response.json();
+            console.log(result);
+                if(result){
+                  setTimeout(()=>{
+                    console.log("timeout")
+                    getGroceryItem(uniqueId);
+                  },5000)
+                   
+                }
+
+
+                setAckData(tempJosn);
+            }
+    const getGroceryItem = async(uniqueId) => {
+        try {
+            //const response = await fetch("https://api.aabglaunchpadindiademo.com/getSearchData", {
+            const response = await fetch("https://api.aabglaunchpadindiademo.com/getSearchDataManual", {
+            
+            method: "POST",
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            // body: JSON.stringify({
+            //     "transaction_id": uniqueId
+                
+            // })
+            body: JSON.stringify({
+              "category_id": categoryItem
+              
+          })
+          })
+           
+           const data = await response.json();
+           if(data){
+            setFinalData(data);
+            setLoader(false);
+            console.log("resultGetData==============="+JSON.stringify(data));
+            console.log("Searched DATA=========================="+data);
+            console.log("Searched DATA=========================="+JSON.stringify(data));
+            setSearchedData(JSON.stringify(data))
+            
+            navigate("/grocery");
+           }
+           
+           console.log("I am here");
+         } catch (error) {
+          setLoader(false);
+           console.log(error);
+         }
+
+    }
+    const getGroceryData = (category)=>{
+        console.log("get data",category);
+        categoryItem = category;
+        console.log(categoryItem)
+        setCatName(categoryItem);
+        ackDataPrepare();
+    }
+
+
     return(
-        <div>
-            <div className="offcanvas offcanvas-start w-20 menu-container" tabIndex="-1" id="offcanvas" data-bs-keyboard="false" data-bs-backdrop="false">
-            <div className="offcanvas-header">
-            <h3 className="offcanvas-title d-none d-sm-block" id="offcanvas">DT Store</h3>
-            <button type="button" className="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-            </div>
-            <div className="offcanvas-body px-0">
-            <ul className="nav nav-pills flex-column mb-sm-auto mb-0 align-items-start" id="menu">
-                {/* <li className="nav-item userList">
-                    <span className="userProfile">SS</span>
-                    <span>Signed in as</span>
-                    <span className="logout-icon"><i className="bi bi-box-arrow-right"></i></span>
-                    
-                </li> */}
-                <li className="nav-item navList">
-                    <Link to="/grocerySubCategory" className="nav-link text-truncate" data-bs-dismiss="offcanvas">
-                        Grocery
-                    </Link>
-                </li>
-                <li className="nav-item navList">
-                    <Link to="/electronicSubCategory" className="nav-link text-truncate" data-bs-dismiss="offcanvas">
-                        Electronics
-                    </Link>
-                    </li>
-                <li className="nav-item navList">
-                    <Link to="/faishonSubCategory" className="nav-link text-truncate" data-bs-dismiss="offcanvas">
-                        Fashion
-                    </Link>
-                </li>
-            </ul>
-            </div>
+        <>
+       {
+        loader && <div id="semiTransparenDiv"></div>   
+       }    
+
+            <div className='container-fluid page-container'> 
+        <div className='page-title'>
+            <h3>Welcome to DT Store</h3>
         </div>
-       
-        </div>
+            <div className="row home-container">
+            <div className="col-md-3">
+                    <div className="round-card">
+                        <div className='card-content' onClick={()=>getGroceryData('Headset')}>
+                            <Link  className="" data-bs-dismiss="offcanvas">
+                                <img src={Headset} className='' width={'100%'}/>
+                            </Link>
+                            
+                        </div>
+                    </div>
+                    <div className='icon-title'><h5>Headset</h5></div>
+                </div>
+            <div className="col-md-3">
+                    <div className="round-card">
+                        <div className='card-content' onClick={()=>getGroceryData('Mobile Phone')}>
+                            <Link  className="" data-bs-dismiss="offcanvas">
+                                <img src={MobilePhone} className='' width={'100%'}/>
+                            </Link>
+                            
+                        </div>
+                    </div>
+                    <div className='icon-title'><h5>Mobile Phone</h5></div>
+                </div>
+                <div className="col-md-3">
+                    <div className="round-card">
+                        <div className='card-content' onClick={()=>getGroceryData('Laptop')}>
+                            <Link  className="" data-bs-dismiss="offcanvas">
+                                <img src={Laptop} className='' width={'100%'}/>
+                            </Link>
+                            
+                        </div>
+                    </div>
+                    <div className='icon-title'><h5>Laptop</h5></div>
+                </div>
+                
+                <div className="col-md-3">
+                    <div className="round-card">
+                        <div className='card-content' onClick={()=>getGroceryData('Desktop')}>
+                            <Link  className="" data-bs-dismiss="offcanvas">
+                                <img src={Desktop} className='' width={'100%'}/>
+                            </Link>
+                            
+                        </div>
+                    </div>
+                    <div className='icon-title'><h5>Desktop</h5></div>
+                </div>
+                
+                
+            </div> 
+      </div>
+        </>
     )
 }
 
-export default Menu;
+export default ElectronicSubCategory;
