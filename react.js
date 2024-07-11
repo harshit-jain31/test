40b150ba2649eb9503a50a380b07627ea11bd6f0
harshit-jain31 <169134156+harshit-jain31@users.noreply.github.com>
2024-07-11 16:24:59 +0530

    Update react.js

--- react.js
+++ react.js
@@ -1,11 +1,3 @@
-import { Link } from "react-router-dom";
-import MobilePhone from '../images/Mobile.jpg';
-import Laptop from '../images/Laptop.jpg';
-import Headset from '../images/Headphones.jpg';
-import Desktop from '../images/DesktopNew.jpg';
-import { useEffect, useState } from "react";
-import { v4 as uuidv4 } from 'uuid';
-import { useNavigate } from "react-router-dom";
 function ElectronicSubCategory({setSearchedData}) {
     const [ackData,setAckData] = useState();
     const [catName,setCatName] = useState('');
